2f998500ded5025885fd0cf34caf8de36603c94f
David Escribano <david.escribanor@gmail.com>
2020-05-24 21:21:12 +0200

    Added new dictionary codes

--- src/utils/dictionary-codes.js
+++ src/utils/dictionary-codes.js
@@ -1,8 +1,14 @@
 module.exports = {
     validation: {
         MUST_NOT_BE_EMPTY: 'MUST_NOT_BE_EMPTY',
-        MUST_BE_ARRAY: 'MUST_BE_ARRAY',
+        MUST_BE_ARRAY_9_ELEMENTS: 'MUST_BE_ARRAY_9_ELEMENTS',
         MUST_BE_BOOLEAN: 'MUST_BE_BOOLEAN',
-        MUST_BE_MAX_99_CHARS: 'MUST_BE_MAX_9_CHARS'
+        MUST_BE_MAX_99_CHARS: 'MUST_BE_MAX_9_CHARS',
+        MUST_BE_X_OR_O: 'MUST_BE_X_OR_O'
+    },
+    gameStatus: {
+        WINNER: 'WINNER',
+        DRAW: 'DRAW',
+        GAME_IN_PROGRESS: 'GAME_IN_PROGRESS'
     }
 }
