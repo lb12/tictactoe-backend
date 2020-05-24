ac1fbb304244ff69bd8cd391b7f94d6c4bae6609
David Escribano <david.escribanor@gmail.com>
2020-05-24 21:20:54 +0200

    Improved PlaysValidation

--- src/middlewares/validations/PlaysValidation/index.js
+++ src/middlewares/validations/PlaysValidation/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Node imports
-const { body } = require("express-validator");
+const { body, check } = require("express-validator");
 
 // Own imports
 const { validation: codes } = require('../../../utils/dictionary-codes');
@@ -9,9 +9,11 @@ const { validation: codes } = require('../../../utils/dictionary-codes');
 module.exports = {
     getNewPlayValidation: [
         body('board')
-            .exists({checkFalsy: true, checkNull: true}).withMessage(codes.MUST_NOT_BE_EMPTY)
-            .isArray().withMessage(codes.MUST_BE_ARRAY),
-            // .isLength({ min: 9, max: 9 }).withMessage(codes.MUST_BE_MAX_9_CHARS),
+            .exists({ checkFalsy: true, checkNull: true }).withMessage(codes.MUST_NOT_BE_EMPTY)
+            .isArray({ max: 9 }).withMessage(codes.MUST_BE_ARRAY_9_ELEMENTS),
+        check('board.*')
+            .isString().isLength({ min: 0, max: 1 }).withMessage(codes.MUST_BE_MAX_9_CHARS)
+            .matches(/[XO]/).withMessage(codes.MUST_BE_X_OR_O),
         body('isPlayerX')
             .exists({checkFalsy: true, checkNull: true}).withMessage(codes.MUST_NOT_BE_EMPTY)
             .isBoolean().withMessage(codes.MUST_BE_BOOLEAN)
