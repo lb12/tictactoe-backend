2785accb34c67970c64fab2963ff2c06d1b93285
David Escribano Rodríguez <david.escribanor@gmail.com>
2023-12-03 16:55:58 +0100

    chore: add ecosystem pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'tictactoe-backend',
-      script: 'node_modules/.bin/npm',
-      args: 'run start', // Reemplaza 'start' con el script que deseas ejecutar
-      exec_mode: 'cluster',
+      script: 'npm',
+      args: 'start', // Reemplaza 'start' con el script que deseas ejecutar
       log_date_format: 'YYYY-MM-DD HH:mm:ss', // Formato de fecha y hora
     },
   ],
